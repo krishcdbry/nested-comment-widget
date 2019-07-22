da3b9a166feeaea209d69f98231f02d673cd9539
krishcdbry <krishcdbry@gmail.com>
2019-07-23 00:03:48 +0530

    Updated

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ let comments = [];
 let commentList = document.getElementById('comments-list');
 let oldIdx = 0;
 
+/**
+ * Takes care of random ID's
+ */
 const _getRandomID = () => {
   oldIdx = (new Date()).getTime()+oldIdx+1;
   return oldIdx;
 }
 
+/**
+ * Takes care of storeupdation
+ * @param {*} comments  // List of comments
+ */
 const _updateStore = (comments) => {
   window.localStorage.setItem('comments', JSON.stringify(comments));
 }
 
+/**
+ * Prepared a valid comment object
+ * @param {*} comment  
+ */
 const _prepareCommentObj = (comment) => {
   return {
     comment,
@@ -20,6 +31,11 @@ const _prepareCommentObj = (comment) => {
   }
 }
 
+/**
+ * Prepare comment box
+ * @param {*} commentId   // Comment index
+ * @param {*} replyDivId  // Replies element index
+ */
 const _prepareCommentBox = (commentId, replyDivId) => {
   let commentBox = document.createElement('DIV');
   let textarea = document.createElement('TEXTAREA');
@@ -42,6 +58,10 @@ const _prepareCommentBox = (commentId, replyDivId) => {
   return commentBox;
 }
 
+/**
+ * Prepares comment for rendering
+ * @param {*} commentObj 
+ */
 const _prepareComment = (commentObj) => {
 
   const {id, user, comment, reply} = commentObj;
@@ -99,29 +119,49 @@ const _prepareComment = (commentObj) => {
   return commentDiv;
 }
 
-
-function iterateComments(comments, reply, commentId) {
+/**
+ * Loop through comments and updates them
+ * @param {*} comments  // Comments list
+ * @param {*} reply     // Reply item to be added
+ * @param {*} commentId 
+ */
+function findAndUpdateComments(comments, reply, commentId) {
   return comments.map(item => {
     if (item.id === commentId) {
       item.reply.push(reply);
     }
     else {
-      item.reply = iterateComments(item.reply, reply, commentId);
+      item.reply = findAndUpdateComments(item.reply, reply, commentId);
     }
     return item;
   })
 }
 
+/**
+ * Renders comment after adding
+ * @param {*} comment 
+ */
 function renderComment(comment) {
   let commentElement = _prepareComment(comment);
   commentList.appendChild(commentElement);
 }
 
+/**
+ * Renders reply after adding
+ * @param {*} reply   // reply object
+ * @param {*} replyDivId  // reply div ID
+ */
 function renderReply(reply, replyDivId) {
   let replyElement = _prepareComment(reply);
   document.getElementById(replyDivId).appendChild(replyElement);
 }
 
+/**
+ * Adding reply to the comment
+ * @param {*} reply      // Reply message
+ * @param {*} commentId  // Comment id
+ * @param {*} replyDivId  // Reply div id
+ */
 function addReply(reply, commentId, replyDivId) {
   let replyObj = _prepareCommentObj(reply);
   comments = iterateComments(comments, replyObj, commentId);
@@ -129,6 +169,9 @@ function addReply(reply, commentId, replyDivId) {
   _updateStore(comments);
 }
 
+/**
+ * Adding comment
+ */
 function addComment() {
   let comment = document.getElementById('comment-box').value;
   let commentObj = _prepareCommentObj(comment);
@@ -137,7 +180,9 @@ function addComment() {
   _updateStore(comments);
 }
 
-
+/**
+ * Inti function
+ */
 function _init() {
   if (!window.localStorage.getItem('comments')) {
     window.localStorage.setItem('comments', JSON.stringify([]));
